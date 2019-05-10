c8114a84334fe5e7179286e1d4a9ab4d8139a35a
Patrick McGehearty <patrick.mcgehearty@oracle.com>
2019-05-10 10:26:31 -0700

    glibc-rh1540480-7.patch

--- elf/dl-load.c
+++ elf/dl-load.c
@@ -550,7 +550,6 @@ decompose_rpath (struct r_search_path_struct *sps,
 {
   /* Make a copy we can work with.  */
   const char *where = l->l_name;
-  char *copy;
   char *cp;
   struct r_search_path_elem **result;
   size_t nelems;
@@ -589,20 +588,19 @@ decompose_rpath (struct r_search_path_struct *sps,
       while (*inhp != '\0');
     }
 
+  /* Ignore empty rpaths.  */
+  if (*rpath == '\0')
+    {
+      sps->dirs = (struct r_search_path_elem **) -1;
+      return false;
+    }
+
   /* Make a writable copy.  */
-  copy = local_strdup (rpath);
+  char *copy = local_strdup (rpath);
   if (copy == NULL)
     {
       errstring = N_("cannot create RUNPATH/RPATH copy");
       goto signal_error;
-    }
-
-  /* Ignore empty rpaths.  */
-  if (*copy == 0)
-    {
-      free (copy);
-      sps->dirs = (struct r_search_path_elem **) -1;
-      return false;
     }
 
   /* Count the number of necessary elements in the result array.  */

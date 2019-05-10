6ecae4101da1e310d64ab521869477a2b8c15336
Patrick McGehearty <patrick.mcgehearty@oracle.com>
2019-05-10 10:26:26 -0700

    glibc-rh1560641.patch

--- nptl/sem_open.c
+++ nptl/sem_open.c
@@ -313,6 +313,9 @@ sem_open (const char *name, int oflag, ...)
       sem.newsem.value = value << SEM_VALUE_SHIFT;
       sem.newsem.nwaiters = 0;
 #endif
+      /* pad is used as a mutex on pre-v9 sparc and ignored otherwise.  */
+      sem.newsem.pad = 0;
+
       /* This always is a shared semaphore.  */
       sem.newsem.private = LLL_SHARED;
 

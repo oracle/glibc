8328e987db634d7bb2837546d88a46d41b3bf399
Patrick McGehearty <patrick.mcgehearty@oracle.com>
2019-05-10 10:21:57 -0700

    glibc-rh731837-16.patch

--- sysdeps/powerpc/powerpc64/multiarch/wcsrchr-power6.c
+++ sysdeps/powerpc/powerpc64/multiarch/wcsrchr-power6.c
@@ -1,6 +1,6 @@
-/* Copyright (C) 1995, 1996, 1999 Free Software Foundation, Inc.
+/* wcsrchr.c - Wide Character Search for powerpc64/power6.
+   Copyright (C) 2013-2014 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
-   Contributed by Ulrich Drepper, <drepper@gnu.ai.mit.edu>
 
    The GNU C Library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
@@ -13,27 +13,7 @@
    Lesser General Public License for more details.
 
    You should have received a copy of the GNU Lesser General Public
-   License along with the GNU C Library; if not, see
-   <http://www.gnu.org/licenses/>.  */
+   License along with the GNU C Library; see the file COPYING.LIB.  If
+   not, see <http://www.gnu.org/licenses/>.  */
 
-#include <wchar.h>
-
-#ifndef WCSRCHR
-# define WCSRCHR wcsrchr
-#endif
-
-/* Find the last occurrence of WC in WCS.  */
-wchar_t *
-WCSRCHR (wcs, wc)
-     register const wchar_t *wcs;
-     register const wchar_t wc;
-{
-  const wchar_t *retval = NULL;
-
-  do
-    if (*wcs == wc)
-      retval = wcs;
-  while (*wcs++ != L'\0');
-
-  return (wchar_t *) retval;
-}
+#include <sysdeps/powerpc/powerpc32/power4/multiarch/wcsrchr-power6.c>

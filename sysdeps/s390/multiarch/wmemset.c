fc25a280c75ef7fe83521f95344034bbf4f49834
Patrick McGehearty <patrick.mcgehearty@oracle.com>
2020-01-14 19:53:45 +0000

    glibc-rh1268008-28.patch

--- sysdeps/s390/multiarch/wmemset.c
+++ sysdeps/s390/multiarch/wmemset.c
@@ -1,6 +1,6 @@
-/* Copyright (C) 1996,97,99,2002 Free Software Foundation, Inc.
+/* Multiple versions of wmemset.
+   Copyright (C) 2015 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
-   Contributed by Ulrich Drepper <drepper@gnu.org>, 1996.
 
    The GNU C Library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
@@ -16,43 +16,12 @@
    License along with the GNU C Library; if not, see
    <http://www.gnu.org/licenses/>.  */
 
-#include <wchar.h>
+#if defined HAVE_S390_VX_ASM_SUPPORT && !defined NOT_IN_libc
+# include <wchar.h>
+# include <ifunc-resolve.h>
 
-#ifdef WMEMSET
-# define wmemset WMEMSET
-#endif
+s390_vx_libc_ifunc2 (__wmemset, wmemset)
 
-wchar_t *
-wmemset (s, c, n)
-     wchar_t *s;
-     wchar_t c;
-     size_t n;
-{
-  register wchar_t *wp = s;
-
-  while (n >= 4)
-    {
-      wp[0] = c;
-      wp[1] = c;
-      wp[2] = c;
-      wp[3] = c;
-      wp += 4;
-      n -= 4;
-    }
-
-  if (n > 0)
-    {
-      wp[0] = c;
-
-      if (n > 1)
-	{
-	  wp[1] = c;
-
-	  if (n > 2)
-	    wp[2] = c;
-	}
-    }
-
-  return s;
-}
-libc_hidden_def (wmemset)
+#else
+# include <wcsmbs/wmemset.c>
+#endif /* !(defined HAVE_S390_VX_ASM_SUPPORT && !defined NOT_IN_libc) */

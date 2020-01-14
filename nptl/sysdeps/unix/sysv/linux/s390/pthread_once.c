0b438590f5a70b8bfee9c84a128866336badd6cf
Patrick McGehearty <patrick.mcgehearty@oracle.com>
2020-01-14 19:55:49 +0000

    glibc-rh1505492-prototypes-s390-pthread_once.patch

--- nptl/sysdeps/unix/sysv/linux/s390/pthread_once.c
+++ nptl/sysdeps/unix/sysv/linux/s390/pthread_once.c
@@ -34,9 +34,7 @@ clear_once_control (void *arg)
 
 
 int
-__pthread_once (once_control, init_routine)
-     pthread_once_t *once_control;
-     void (*init_routine) (void);
+__pthread_once (pthread_once_t *once_control, void (*init_routine) (void))
 {
   while (1)
     {

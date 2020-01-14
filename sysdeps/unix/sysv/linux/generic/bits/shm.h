4ef838becd2ef136afb4efb74a87dae933c53d51
Patrick McGehearty <patrick.mcgehearty@oracle.com>
2020-01-14 19:58:01 +0000

    marvell-Change-shm_segsz-to-be-__syscall_ulong_t.patch

--- sysdeps/unix/sysv/linux/generic/bits/shm.h
+++ sysdeps/unix/sysv/linux/generic/bits/shm.h
@@ -51,24 +51,24 @@ typedef unsigned long int shmatt_t;
 struct shmid_ds
   {
     struct ipc_perm shm_perm;		/* operation permission struct */
-    size_t shm_segsz;			/* size of segment in bytes */
+    __syscall_ulong_t shm_segsz;	/* size of segment in bytes */
     __time_t shm_atime;			/* time of last shmat() */
 #if __WORDSIZE == 32
-    unsigned long int __unused1;
+    __syscall_ulong_t __unused1;
 #endif
     __time_t shm_dtime;			/* time of last shmdt() */
 #if __WORDSIZE == 32
-    unsigned long int __unused2;
+    __syscall_ulong_t __unused2;
 #endif
     __time_t shm_ctime;			/* time of last change by shmctl() */
 #if __WORDSIZE == 32
-    unsigned long int __unused3;
+    __syscall_ulong_t __unused3;
 #endif
     __pid_t shm_cpid;			/* pid of creator */
     __pid_t shm_lpid;			/* pid of last shmop */
     shmatt_t shm_nattch;		/* number of current attaches */
-    unsigned long int __unused4;
-    unsigned long int __unused5;
+    __syscall_ulong_t __unused4;
+    __syscall_ulong_t __unused5;
   };
 
 #ifdef __USE_MISC

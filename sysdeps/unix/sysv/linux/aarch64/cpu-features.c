4673f0cddbecd5ca09bce9968dffb0188a6fdeb6
Patrick McGehearty <patrick.mcgehearty@oracle.com>
2020-01-14 19:57:44 +0000

    glibc-aarch64-Add-emag-to-tunable-cpu-list.patch

--- sysdeps/unix/sysv/linux/aarch64/cpu-features.c
+++ sysdeps/unix/sysv/linux/aarch64/cpu-features.c
@@ -35,6 +35,9 @@ static struct cpu_list cpu_list[] = {
       {"thunderxt88",	 0x430F0A10},
       {"thunderx2t99",   0x431F0AF0},
       {"thunderx2t99p1", 0x420F5160},
+      {"phecda",	 0x680F0000},
+      {"ares",		 0x411FD0C0},
+      {"emag",		 0x503F0001},
       {"generic", 	 0x0}
 };
 

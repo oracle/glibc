43f67ad2301d0ab7b825d99539a8ea3bbf7f6e6a
Patrick McGehearty <patrick.mcgehearty@oracle.com>
2020-07-24 19:49:24 +0000

    glibc-rh1150282.patch

--- sysdeps/i386/i686/multiarch/strstr-c.c
+++ sysdeps/i386/i686/multiarch/strstr-c.c
@@ -1,8 +1,6 @@
 /* Multiple versions of strstr
    All versions must be listed in ifunc-impl-list.c.  */
 
-#include "init-arch.h"
-
 #define STRSTR __strstr_ia32
 #if defined SHARED && defined DO_VERSIONING && !defined NO_HIDDEN
 #undef libc_hidden_builtin_def
@@ -17,13 +15,17 @@
 
 #include "string/strstr.c"
 
+#include "init-arch.h"
+
 extern __typeof (__redirect_strstr) __strstr_sse42 attribute_hidden;
 extern __typeof (__redirect_strstr) __strstr_ia32 attribute_hidden;
 
 /* Avoid DWARF definition DIE on ifunc symbol so that GDB can handle
    ifunc symbol properly.  */
 extern __typeof (__redirect_strstr) __libc_strstr;
-libc_ifunc (__libc_strstr, HAS_SSE4_2 ? __strstr_sse42 : __strstr_ia32)
+libc_ifunc (__libc_strstr,
+	    HAS_SSE4_2 && !use_unaligned_strstr () ?
+	    __strstr_sse42 : __strstr_ia32)
 
 #undef strstr
 strong_alias (__libc_strstr, strstr)

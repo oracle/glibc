ff3e2fb382d621877b4fc79f6a56161dce734c2a
Patrick McGehearty <patrick.mcgehearty@oracle.com>
2020-10-22 13:18:08 -0700

    glibc-rh1505492-prototypes-memchr.patch

--- string/memchr.c
+++ string/memchr.c
@@ -57,10 +57,7 @@
 
 /* Search no more than N bytes of S for C.  */
 __ptr_t
-MEMCHR (s, c_in, n)
-     const __ptr_t s;
-     int c_in;
-     size_t n;
+MEMCHR (const __ptr_t s, int c_in, size_t n)
 {
   const unsigned char *char_ptr;
   const unsigned long int *longword_ptr;

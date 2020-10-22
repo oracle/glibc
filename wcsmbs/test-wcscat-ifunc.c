4a2ff88da405a971656f4b291a9f5160ec941b81
Patrick McGehearty <patrick.mcgehearty@oracle.com>
2020-10-22 13:17:08 -0700

    glibc-rh1268008-16.patch

--- wcsmbs/test-wcscat-ifunc.c
+++ wcsmbs/test-wcscat-ifunc.c
@@ -1,4 +1,5 @@
-/* Copyright (C) 1991, 1997, 2003 Free Software Foundation, Inc.
+/* Test and measure IFUNC implementations of wcscat function.
+   Copyright (C) 2015 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -15,39 +16,5 @@
    License along with the GNU C Library; if not, see
    <http://www.gnu.org/licenses/>.  */
 
-#include <string.h>
-#include <memcopy.h>
-
-#undef strcat
-
-#ifndef STRCAT
-# define STRCAT strcat
-#endif
-
-/* Append SRC on the end of DEST.  */
-char *
-STRCAT (char *dest, const char *src)
-{
-  char *s1 = dest;
-  const char *s2 = src;
-  char c;
-
-  /* Find the end of the string.  */
-  do
-    c = *s1++;
-  while (c != '\0');
-
-  /* Make S1 point before the next character, so we can increment
-     it while memory is read (wins on pipelined cpus).  */
-  s1 -= 2;
-
-  do
-    {
-      c = *s2++;
-      *++s1 = c;
-    }
-  while (c != '\0');
-
-  return dest;
-}
-libc_hidden_builtin_def (strcat)
+#define TEST_IFUNC 1
+#include "test-wcscat.c"

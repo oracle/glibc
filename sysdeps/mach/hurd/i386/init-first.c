cc093ca6de29319fae5c65c751c04d32bba18d0c
Oracle Public Cloud User <opc@pmcgehea-el8-20210617-1646.allregionaliads.osdevelopmeniad.oraclevcn.com>
2022-05-11 15:12:03 +0000

    /home/opc/ol8u6_ga/glibc-rh1817513-50.patch

--- sysdeps/mach/hurd/i386/init-first.c
+++ sysdeps/mach/hurd/i386/init-first.c
@@ -17,7 +17,6 @@
    <http://www.gnu.org/licenses/>.  */
 
 #include <assert.h>
-#include <ctype.h>
 #include <hurd.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -83,9 +82,6 @@ posixland_init (int argc, char **argv, char **envp)
   _dl_non_dynamic_init ();
 #endif
   __init_misc (argc, argv, envp);
-
-  /* Initialize ctype data.  */
-  __ctype_init ();
 
 #if defined SHARED && !defined NO_CTORS_DTORS_SECTIONS
   __libc_global_ctors ();

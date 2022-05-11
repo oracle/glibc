52166b11d2879b55bea0d7ab30cef354da0fc4e3
Oracle Public Cloud User <opc@pmcgehea-el8-20210617-1646.allregionaliads.osdevelopmeniad.oraclevcn.com>
2022-05-11 15:12:22 +0000

    /home/opc/ol8u6_ga/glibc-rh1991001-17.patch

--- elf/rtld.c
+++ elf/rtld.c
@@ -1693,7 +1693,7 @@ ERROR: '%s': cannot process note segment.\n", _dl_argv[0]);
   /* Add the dynamic linker to the TLS list if it also uses TLS.  */
   if (GL(dl_rtld_map).l_tls_blocksize != 0)
     /* Assign a module ID.  Do this before loading any audit modules.  */
-    GL(dl_rtld_map).l_tls_modid = _dl_next_tls_modid ();
+    _dl_assign_tls_modid (&GL(dl_rtld_map));
 
   audit_list_add_dynamic_tag (&state.audit_list, main_map, DT_AUDIT);
   audit_list_add_dynamic_tag (&state.audit_list, main_map, DT_DEPAUDIT);

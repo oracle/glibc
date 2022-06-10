be8ee09fdc8bbb21f13ff1949e074bfb7606843d
Patrick McGehearty <patrick.mcgehearty@oracle.com>
2022-06-10 19:18:16 +0000

    marvell-Backported-to-fix-ltp-set-re-res-g-u-id.patch

---
+++ sysdeps/unix/sysv/linux/mips/setreuid.c
@@ -0,0 +1,37 @@
+/* Copyright (C) 1998, 2000, 2003, 2004, 2006 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307 USA.  */
+
+#include <errno.h>
+#include <unistd.h>
+#include <setxid.h>
+
+/* Arguments are passed as longs to system calls. Since the arguments of
+   this function are of type "unsigned int" they get zero-extended during
+   the coversion to long. The kernel does not convert them back to
+   "unsigned int" i.e. does not truncate the values but assumes that they
+   are already unsigned ints. By converting to "int" we avoid the
+   zero-extension and thus the value remains in unsigned-int
+   representation.  */
+int
+__setreuid (uid_t ruid, uid_t euid)
+{
+  return INLINE_SETXID_SYSCALL (setreuid, 2, (int) ruid, (int) euid);
+}
+#ifndef __setreuid
+weak_alias (__setreuid, setreuid)
+#endif

a6419856b07675365fde11b12c3b2940db82e642
Patrick McGehearty <patrick.mcgehearty@oracle.com>
2022-06-10 19:14:24 +0000

    glibc-rh1418978-max_align_t.patch

--- include/sys/cdefs.h
+++ include/sys/cdefs.h
@@ -15,6 +15,18 @@ rtld_hidden_proto (__chk_fail)
 
 
 # define __attribute_alloc_size(...) __attribute__ ((alloc_size (__VA_ARGS__)))
+
+/* This mirrors the C11/C++11 max_align_t type provided by GCC, but it
+   is also available in C99 mode.  The aligned attributes are required
+   because some ABIs have reduced alignment requirements for struct and
+   union members.  */
+#if __STDC_VERSION__ < 201112L && __cplusplus < 201103L
+typedef struct {
+  long long ll __attribute__ ((__aligned__ (__alignof__ (long long))));
+  long double ld __attribute__ ((__aligned__ (__alignof__ (long double))));
+} max_align_t;
+#endif /* __STDC_VERSION__ < 201112 && __cplusplus < 201103L */
+
 #endif
 
 #endif

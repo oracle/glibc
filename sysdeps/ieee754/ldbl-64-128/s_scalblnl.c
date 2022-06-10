0e312c0a82f29101892b400a862ee083d2a61731
Patrick McGehearty <patrick.mcgehearty@oracle.com>
2022-06-10 19:14:16 +0000

    glibc-rh1256317-5.patch

--- sysdeps/ieee754/ldbl-64-128/s_scalblnl.c
+++ sysdeps/ieee754/ldbl-64-128/s_scalblnl.c
@@ -2,7 +2,7 @@
 #undef weak_alias
 #define weak_alias(n,a)
 #include <sysdeps/ieee754/ldbl-128/s_scalblnl.c>
-#ifdef IS_IN_libm
+#if IS_IN (libm)
 long_double_symbol (libm, __scalblnl, scalblnl);
 #else
 long_double_symbol (libc, __scalblnl, scalblnl);

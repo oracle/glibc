217b31b54b83bd8d4f24c48c47910930bd63a5f8
Patrick McGehearty <patrick.mcgehearty@oracle.com>
2022-06-10 19:13:53 +0000

    glibc-rh1268008-29.patch

--- sysdeps/s390/multiarch/ifunc-impl-list.c
+++ sysdeps/s390/multiarch/ifunc-impl-list.c
@@ -135,6 +135,8 @@ __libc_ifunc_impl_list (const char *name, struct libc_ifunc_impl *array,
 
   IFUNC_VX_IMPL (wmemset);
 
+  IFUNC_VX_IMPL (wmemcmp);
+
 #endif /* HAVE_S390_VX_ASM_SUPPORT */
 
   return i;

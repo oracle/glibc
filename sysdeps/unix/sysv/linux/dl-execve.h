ff6e98761fb5de964581e0c866f4ffb01d9ee1b4
Cupertino Miranda <cupertino.miranda@oracle.com>
2022-12-07 22:00:36 +0000

    644 - glibc-rh2023420-6.patch

---
+++ sysdeps/unix/sysv/linux/dl-execve.h
@@ -0,0 +1,30 @@
+/* execve for the dynamic linker.  Linux version.
+   Copyright (C) 2021 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <errno.h>
+
+static inline int
+__rtld_execve (const char *path, char *const *argv, char *const *envp)
+{
+  INTERNAL_SYSCALL_DECL (err);
+  long int r = INTERNAL_SYSCALL_CALL (execve, err, path, argv, envp);
+  if (INTERNAL_SYSCALL_ERROR_P (r, err))
+    return INTERNAL_SYSCALL_ERRNO (r, err);
+  else
+    return 0;
+}

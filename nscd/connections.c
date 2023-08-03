64bb8c66058807230ceaf7b687421ea5c64cfd0f
Cupertino Miranda <cupertino.miranda@oracle.com>
2023-08-03 14:27:39 +0000

    780 - glibc-rh2122498.patch

--- nscd/connections.c
+++ nscd/connections.c
@@ -2286,7 +2286,8 @@ main_loop_epoll (int efd)
 					     sizeof (buf))) != -1)
 	      ;
 
-	    __bump_nl_timestamp ();
+	    dbs[hstdb].head->extra_data[NSCD_HST_IDX_CONF_TIMESTAMP]
+	      = __bump_nl_timestamp ();
 	  }
 # endif
 	else

7f698dd3c293f3469c740987ac7fc16b61e21b00
Cupertino Miranda <cupertino.miranda@oracle.com>
2023-08-03 17:30:26 +0000

    123 - glibc-rh2024347-7.patch

--- nptl/descr.h
+++ nptl/descr.h
@@ -35,6 +35,7 @@
 #include <bits/types/res_state.h>
 #include <kernel-features.h>
 #include <tls-internal-struct.h>
+#include <sys/rseq.h>
 
 #ifndef TCB_ALIGNMENT
 # define TCB_ALIGNMENT 32
@@ -406,6 +407,9 @@ struct pthread
 
   /* Used on strsignal.  */
   struct tls_internal_t tls_state;
+
+  /* rseq area registered with the kernel.  */
+  struct rseq rseq_area;
 
   /* This member must be last.  */
   char end_padding[];
